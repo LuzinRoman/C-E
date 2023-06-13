3e78f759480323ec4c569a99c6cfa071d7c755fd
Roman <rluzin@yandex.ru>
2023-06-13 18:32:33 +0300

    Part2

---
+++ task11/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int randomNumber = new Random().Next(100, 1000);
+int newNumber = DeleteSecondDigit(randomNumber);
+
+Console.WriteLine($"Рандомное случайное число => {randomNumber}");
+Console.WriteLine($"Полученное двухзначное число => {newNumber}");
+
+int DeleteSecondDigit(int randomNum)
+
+{
+    int firstDigit = randomNum / 100;
+    int secondDigit = randomNum % 10;
+    int newNumber = (firstDigit * 10) + secondDigit;
+    return newNumber;
+}
+
+// int number = new Random().Next(100, 1000);
+// int temp = number / 100;
+// int tempOne = number % 10;
+// Console.Write(number + " " + temp + tempOne);
---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 % number1 == 0)
+{
+    Console.WriteLine($"Число {number2} кратно {number1}");
+
+      
+    }
+else
+{
+    Console.WriteLine("Не кратно, остаток" + (number1 % number2));
+}
