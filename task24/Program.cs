52d92854a378878332cbd07de55605b9837390d3
Roman <rluzin@yandex.ru>
2023-06-21 13:58:46 +0300

    разобрали task24

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int GetSum(int a)
+{
+    int sum = 0;
+for(int i = 1; i<=a; i++)
+{
+    sum +=i;
+}
+return(sum);
+}
+
+int number = Prompt("Введите число: ");
+int result = GetSum(number);
+Console.WriteLine("Сумма чисел от 1 до А = " + result);
+// int sum = 0;
+
+
+// for(int i = 1; i<=number; i++)
+// {
+//     sum +=i;
+// }
+
