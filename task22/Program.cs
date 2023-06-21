cab54e61e56313b9655ec860a6f84c3ad7af74b9
Roman <rluzin@yandex.ru>
2023-06-21 13:13:43 +0300

    homeworktask19

--- task22/Program.cs
+++ task22/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int number = Prompt("Введите число: ");
+Square(number);
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int i = 1;
+void Square(int number)
+while (i<=number)
+{
+    Console.Write(i*i + " ");
+    i++;
+}
