b7ecc7d440c482a26828f48482fc581382c075a8
Roman <rluzin@yandex.ru>
2023-06-21 13:29:32 +0300

    homeworktask23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int num = Prompt("Введите число: ");
+Square(num);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Square(int number)
+{
+int i = 1;
+while (i<=number)
+{
+    Console.Write(i*i*i + " ");
+    i++;
+}
+}
