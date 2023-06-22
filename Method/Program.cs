81f1747477f752c787052f4f6f3b3d47178d5ece
Roman <rluzin@yandex.ru>
2023-06-22 16:06:37 +0300

    разобрали task28

---
+++ Method/Program.cs
@@ -0,0 +1,51 @@
+﻿// Вид 1
+
+void Method ()
+{
+    Console.WriteLine ("Меня зовут...");
+}
+// Method();
+
+ // Вид2
+void Method2(string msg)
+{
+    Console.WriteLine (msg);
+}
+// Method2(msg: "Меня зовут...");
+
+void Method21(string msg, int count)
+{
+   int i = 0;
+   while (i < count)
+   {
+    Console.WriteLine(msg);
+    i++;
+   }
+}
+// Method21(msg: "Меня зовут...", count: 5);
+
+// Вид3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+// int Year = Method3();
+// Console.WriteLine(Year);
+
+// Вид 4
+
+string Method4 (int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
---
+++ task28/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+// Console.WriteLine("Введите положительное число: ");
+// int number = int.Parse(Console.ReadLine());
+
+
+// int Factorial(int number)
+// {while (number>=1)
+// {
+//   for (int i = 1; number>=1; i++);
+// return num;
+// }
+// }
+
+// int num = Factorial(int );
+// Console.WriteLine(num);
+
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number <=0)
+{
+    Console.WriteLine("Введи положительное цело число, больше нуля");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int result = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++) checked {fact *= i;} 
+    return fact;
+}
