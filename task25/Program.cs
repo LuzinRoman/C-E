3c5190cbe1d883db89103248fcf7e669cf5d926a
Roman <rluzin@yandex.ru>
2023-06-25 12:52:38 +0300

    homework task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,32 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+
+Main();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int GetDigits(int A, int B)
+
+{
+    int result = 1;
+for(int i = 1; i<=B; i++)
+{
+    result = result * A;
+}
+return(result);
+}
+
+void Main()
+{
+int numberA = Prompt("Введите число А: ");
+int numberB = Prompt("Введите число B: ");
+int result = GetDigits(numberA, numberB);
+Console.WriteLine($"{numberA} в степени {numberB} = " + result);
+}
