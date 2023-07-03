8e96e294816c6eea00a1ab168dacc046677d15e0
Roman <rluzin@yandex.ru>
2023-07-03 17:33:35 +0300

    разобрали task49

---
+++ task49/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть вот так:
+// 1 4 44 2
+// 5 9 4 3
+// 64 4 4 4
+
+int rows = Prompt("Введите колличество строк: ");
+int columns = Prompt("Введите колличество столбцов: ");
+int minValue = Prompt("Введите число для минимального диапазона значений: ");
+int maxValue = Prompt("Введите число для максимального диапазона значений: ");
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+int[,] newArray = NewArray(array);
+PrintArray(newArray);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max); // [0,0] , [0,1] , [0,2]....//[1,0], [1,1] , [1,2]
+
+        }
+    }
+    return result;
+}
+
+
+int[,] NewArray(int[,] array)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            array[i,j] *= array[i,j];
+
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) // GetLength(0) - длина строки
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) // GetLength(1) - длина столбцов
+        {
+            Console.Write(arr[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
