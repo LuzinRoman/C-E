7c30ae240541203015b2798f7136bf5ba7af930a
Roman <rluzin@yandex.ru>
2023-07-09 17:00:52 +0300

    homework task58

---
+++ task58/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+        int[,] matrix1 = { { 2, 4 }, { 3, 2 } };
+        int[,] matrix2 = { { 3, 4 }, { 3, 3 } };
+
+
+  int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+
+        Console.WriteLine("Результирующая матрица:");
+
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                Console.Write("{0} ", resultMatrix[i, j]);
+            }
+            Console.WriteLine();
+        }     
+
+ 
