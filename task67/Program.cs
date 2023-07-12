75244c1d1ecbdd0e5b86235fc6e2bb9bbb940dd8
Roman <rluzin@yandex.ru>
2023-07-12 15:55:50 +0300

    разобрали task67

---
+++ task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать 
+// на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = Prompt("Введите число: ");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int SumDigit (int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return SumDigit(n / 10) + (n % 10);
+}
+
+Console.WriteLine($"{SumDigit(number)}");
